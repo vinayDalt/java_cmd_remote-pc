14d8c5f9f464c1265090974936c075cd93b8feb0
vinay kumar <vinay.dalt@gmail.com>
2018-12-20 12:51:29 +0530

    initial commit

---
+++ cal-client/com/stackroute/client/Client.java
@@ -0,0 +1,10 @@
+package com.stackroute.client;
+import com.stackroute.lib.*;
+public class Client
+{
+	public static void main(String[] args)
+	{
+		Calculator obj= new Calculator();
+		System.out.println("the sum is:" + obj.add(4,5));
+	}	
+}
---
+++ cal-lib/com/stackroute/lib/Calculator.java
@@ -0,0 +1,8 @@
+package com.stackroute.lib;
+
+public class Calculator {
+    public int add(int a,int b)
+    {
+        return (a+b);
+    }
+}
